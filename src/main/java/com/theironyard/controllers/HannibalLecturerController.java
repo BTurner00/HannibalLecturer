f2b6bda8f5d049a106ad170d3f55c6cacba3d793
Ben <bhturne@g.clemson.edu>
2016-06-30 14:01:37 -0400

    Initial Commit

---
+++ src/main/java/com/theironyard/controllers/HannibalLecturerController.java
@@ -0,0 +1,10 @@
+package com.theironyard.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Ben on 6/30/16.
+ */
+@Controller
+public class HannibalLecturerController {
+}
---
+++ src/main/java/com/theironyard/controllers/HannibalLecturerRestController.java
@@ -0,0 +1,50 @@
+package com.theironyard.controllers;
+
+import com.theironyard.entities.Lecturer;
+import com.theironyard.services.LecturerRepository;
+import com.theironyard.services.ReviewRepository;
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+/**
+ * Created by Ben on 6/30/16.
+ */
+@RestController
+public class HannibalLecturerRestController {
+
+    @Autowired
+    LecturerRepository lecturers;
+    @Autowired
+    ReviewRepository reviews;
+
+
+    @PostConstruct
+    public void init() throws SQLException {
+        Server.createWebServer().start();
+    }
+
+
+    @RequestMapping(path="/lecturers", method= RequestMethod.POST)
+    public String postLecturers(String name, String topic, String image) {
+        Lecturer lect = new Lecturer(name, topic, image);
+        lecturers.save(lect);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path="", method=RequestMethod.GET)
+    public Iterable<Lecturer> getLecturers() {
+        Iterable<Lecturer> lects = lecturers.findAll();
+        return lects;
+
+
+    }
+}
